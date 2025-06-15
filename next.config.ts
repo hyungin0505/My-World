b76d1bf1ba5b37aad89d99be7b6a7843a0e890b2
hyungin0505 <hyungin0505@naver.com>
2025-06-15 17:15:08 +0900

    Edit: 깃허브 정적 호스팅 가능하도록

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'My-World'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-};
-export default nextConfig;
+export default {
+  output: 'export',
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  images: { unoptimized: true },
+}
