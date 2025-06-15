ffac7335e77d29c604e624442decba6db1d63541
hyungin0505 <hyungin0505@naver.com>
2025-06-15 16:57:37 +0900

    Edit: 깃허브 정적 호스팅 가능하도록

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export"
-};
+// const nextConfig = {
+//   output: "export"
+// };
 
-export default nextConfig;
+export default {
+  output: 'export'
+};
